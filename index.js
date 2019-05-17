f006c5f675759c3488836e2ecb2c14bf0c2d16d3
Yooni Park <yoonikj@gmail.com>
2019-05-17 04:45:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  var newObj = Object.assign({}, object);
+  delete newObj.key;
+  return newObj;
 }
