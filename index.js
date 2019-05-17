ea578fa3c40dc72841958b77e2018126aa757847
Yooni Park <yoonikj@gmail.com>
2019-05-17 04:41:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, obj, { [key]: value })
+  return Object.assign({}, object, { [key]: value })
 }
